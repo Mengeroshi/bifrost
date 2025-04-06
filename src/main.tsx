470fa3b288623c775ac0d5c18876ffd30dac5393
mengeroshi <dpllgr@hotmail.com>
2025-04-06 03:47:05 -0600

    React router integrated

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { Navbar } from "./components/Navbar.tsx";
+import { Wrappers } from "./Wrappers.tsx";
+import { App } from "./App.tsx";
+import { Login } from "./pages/Login.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <Wrappers>
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<App />} />
+        <Route element={<Navbar />}>
+          <Route index element={<App />} />
+          <Route path="login" element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  </StrictMode>
+  </Wrappers>
 );
