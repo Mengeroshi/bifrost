8aaff6a90b255052e534fec079960f4b019ff761
mengeroshi <dpllgr@hotmail.com>
2025-04-07 06:10:22 -0600

    base Color changed

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export function App() {
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-100px)]">
       <h1 className="text-8xl text-center">Welcome to Bifrost</h1>
       <p className="text-2xl text-center text-main">
-        Click in the asset to check their price performance
+        Click in the assets to check their price performance
       </p>
       <section className="flex flex-wrap gap-2 mt-2">
         {CRYPTOS_TO_TRACK.map((crypto) => (
