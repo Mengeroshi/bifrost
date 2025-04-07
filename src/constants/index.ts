fd15d2acb19269ec5576a7925c636c5de393b778
mengeroshi <dpllgr@hotmail.com>
2025-04-07 06:26:56 -0600

    fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
-import BTC from "../assets/BTC.png";
-import ETH from "../assets/ETH.png";
-import DOGE from "../assets/DOGE.png";
-import SOL from "../assets/SOL.png";
-import XRP from "../assets/XRP.png";
+import BTC from "./assets/BTC.png";
+import ETH from "./assets/ETH.png";
+import DOGE from "./assets/DOGE.png";
+import SOL from "./assets/SOL.png";
+import XRP from "./assets/XRP.png";
 
 export const CRYPTOS_TO_TRACK = [
   { name: "Bitcoin", symbol: "BTC", img: BTC },
