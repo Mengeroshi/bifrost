8e84f11d4e78e086097282b133d964f178ef9ccc
mengeroshi <dpllgr@hotmail.com>
2025-04-07 06:00:38 -0600

    index  page fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router";
 import { Button } from "./components/ui/button";
+import { CRYPTOS_TO_TRACK } from "./constants";
 
 export function App() {
-  return <Button>Default</Button>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-100px)]">
+      <h1 className="text-8xl text-center">Welcome to Bifrost</h1>
+      <p className="text-2xl text-center text-main">
+        Click in the asset to check their price performance
+      </p>
+      <section className="flex flex-wrap gap-2 mt-2">
+        {CRYPTOS_TO_TRACK.map((crypto) => (
+          <Link to={`/performance/${crypto.symbol}`} key={crypto.symbol}>
+            <Button size="lg">
+              <img src={crypto.img} alt={crypto.name} className="size-4" />
+              {crypto.name}
+            </Button>
+          </Link>
+        ))}
+      </section>
+    </div>
+  );
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,13 @@
+import BTC from "../assets/BTC.png";
+import ETH from "../assets/ETH.png";
+import DOGE from "../assets/DOGE.png";
+import SOL from "../assets/SOL.png";
+import XRP from "../assets/XRP.png";
+
 export const CRYPTOS_TO_TRACK = [
-  { name: "Bitcoin", symbol: "BTC" },
-  { name: "Ethereum", symbol: "ETH" },
-  { name: "Binace", symbol: "BNB" },
-  { name: "Solana", symbol: "SOL" },
-  { name: "Tron", symbol: "TRX" },
+  { name: "Bitcoin", symbol: "BTC", img: BTC },
+  { name: "Ethereum", symbol: "ETH", img: ETH },
+  { name: "Dogecoin", symbol: "DOGE", img: DOGE },
+  { name: "Solana", symbol: "SOL", img: SOL },
+  { name: "XRP", symbol: "XRP", img: XRP },
 ] as const;
