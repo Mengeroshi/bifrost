1934eb01283305acb7135d709d021a1b04a20faf
mengeroshi <dpllgr@hotmail.com>
2025-04-07 03:32:52 -0600

    Performance page MVP

--- src/pages/Performance.tsx
+++ src/pages/Performance.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { VersusSelect } from "@/components/VersusSelect";
 import { mock } from "@/mock";
-import { priceFormat } from "@/utils/finance";
+import { RootState } from "@/redux/store";
+import { TCleanedHistoricDataItem } from "@/types";
+import { formatDate } from "@/utils/date";
+import {
+  getPerfomanceDataFromCleanHistoricalItems,
+  priceFormat,
+} from "@/utils/finance";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,6 +19,8 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useSearchParams } from "react-router";
 
 ChartJS.register(
   CategoryScale,
@@ -22,6 +32,8 @@ ChartJS.register(
   Legend
 );
 
+// const newMock = cleanRawHistoricalDataItems(mock);
+
 export const options = {
   responsive: true,
   plugins: {
@@ -29,7 +41,7 @@ export const options = {
       position: "top" as const,
     },
     title: {
-      display: true,
+      display: false,
       text: "Chart.js Line Chart",
     },
     tooltip: {
@@ -61,33 +73,100 @@ export const options = {
   pointStyle: "rect",
 };
 
-const formatDate = (date: string) => {
-  return new Date(date).toLocaleString("en-US", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
+const getDataset = (
+  cleanedHistoricalItems: TCleanedHistoricDataItem[],
+  symbol: string,
+  color: string
+) => {
+  if (!cleanedHistoricalItems) {
+    return {
+      label: symbol,
+      data: [],
+      borderColor: color,
+      backgroundColor: color,
+    };
+  }
+  const data = getPerfomanceDataFromCleanHistoricalItems(
+    cleanedHistoricalItems
+  ).map(({ price, time, percentChange }) => ({
+    x: formatDate(time),
+    y: percentChange,
+    price,
+  }));
+
+  return {
+    label: symbol,
+    data,
+    borderColor: color,
+    backgroundColor: color,
+  };
 };
 
-export const data = {
-  datasets: [
-    {
-      label: "BTC",
-      data: mock.map(({ price, time, percentChange }) => ({
-        x: formatDate(time),
-        y: percentChange,
-        price,
-      })),
-      borderColor: "#FD9745",
-      backgroundColor: "#FD9745",
-    },
-  ],
-};
+const dateOptions = [
+  { label: "Year", key: "year" },
+  { label: "Month", key: "month" },
+  { label: "Week", key: "week" },
+];
 
 export const Performance = () => {
+  const { baseCrypto } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const date = searchParams.get("date") || "year";
+  const vs = searchParams.get("vs") || undefined;
+  const dispatch = useDispatch();
+  const cryptoPrices = useSelector((state: RootState) => state.cryptoPrices);
+  const baseHistoricDataItems = cryptoPrices[baseCrypto as string];
+
   return (
-    <div className="flex flex-items-center justify-center">
-      <Line options={options} data={data} />
+    <div className="flex flex-col flex-items-center justify-center">
+      <h1 className=" text-center text-4xl font-bold">
+        {baseCrypto} {vs ? `vs ${vs}` : ""} performace
+      </h1>
+      <div className=" flex justify-between gap-2">
+        <div className="flex flex-col items-start gap-1">
+          <h3 className="text-2xl">Timespan</h3>
+          <div className="flex gap-2 ">
+            {dateOptions.map((option) => (
+              <Button
+                key={option.key}
+                variant={option.key === date ? "default" : "neutral"}
+                onClick={() => {
+                  setSearchParams({ date: option.key, vs: vs || "" });
+                }}
+              >
+                {option.label}
+              </Button>
+            ))}
+          </div>
+        </div>
+        <VersusSelect
+          onClick={(symbol) => {
+            setSearchParams({ vs: symbol, date });
+          }}
+        />
+      </div>
+
+      <Line
+        options={options}
+        data={{
+          datasets: [
+            getDataset(baseHistoricDataItems, baseCrypto as string, "#FD9745"),
+
+            ...(vs ? [getDataset(cryptoPrices[vs], vs, "#3B82F6")] : []),
+          ],
+        }}
+      />
+      <Button
+        onClick={() => {
+          // dispatch({
+          //   type: "cryptoPrices/setCryptoPrices",
+          //   payload: { cryptoTicker: "BTC", newHistoricDataItems: newMock },
+          // });
+          dispatch({ type: "cryptoPrices/clearCryptoPrices" });
+        }}
+      >
+        Clear Prices
+      </Button>
     </div>
   );
 };
--- src/utils/finance.ts
+++ src/utils/finance.ts
@@ -1,3 +1,5 @@
+import { TAPIHistoricDataItem, TCleanedHistoricDataItem } from "@/types";
+
 export const performanceData = ({
   price_close,
   time_period_start,
@@ -16,4 +18,26 @@ export const performanceData = ({
 
 export const priceFormat = (rawPrice: number) => {
   return rawPrice.toLocaleString("en-US", {});
+};
+
+export const cleanRawHistoricalDataItems = (
+  historicalDataItems: TAPIHistoricDataItem[]
+) => {
+  return historicalDataItems.map(({ time_period_start, price_close }) => ({
+    time_period_start,
+    price_close,
+  }));
+};
+
+export const getPerfomanceDataFromCleanHistoricalItems = (
+  cleanedRawHistoricalDataItems: TCleanedHistoricDataItem[]
+) => {
+  return cleanedRawHistoricalDataItems.map(
+    ({ price_close, time_period_start }, _, thisArray) =>
+      performanceData({
+        price_close,
+        time_period_start,
+        initialClose: thisArray[0].price_close,
+      })
+  );
 };
